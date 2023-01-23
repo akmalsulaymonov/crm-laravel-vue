894fc0be6673b0275ae2a3180485fc7aa0e5414e
akmalsulaymonov <akmalsulaymonov@gmail.com>
2023-01-23 17:05:12 +0500

    Roles and extract functionality into its own components

--- resources/js/pages/users/UserList.vue
+++ resources/js/pages/users/UserList.vue
@@ -1,17 +1,16 @@
 <script setup>
     import axios from 'axios';
     import { ref, onMounted, reactive } from 'vue';
-    import { Form, Field } from 'vee-validate';
+    import { Form, Field, useResetForm } from 'vee-validate';
     import * as yup from 'yup';
     import { useToastr } from '../../toastr.js';
-    import { formatDate } from '../../helper.js'
+    import UserListItem from './UserListItem.vue';
 
     const toastr = useToastr();
     const users = ref([]);
     const editing = ref(false);
     const formValues = ref();
     const form = ref(null);
-    const userID = ref(null);
 
     // get list of users
     const getUsers = () => {
@@ -95,19 +94,10 @@
         }
     }
 
-    const deleteUserForm = (user) => {
-        userID.value = user.id;
-        $('#userDeleteFormModal').modal('show');
-    }
-
-    const deleteUser = () => {
-        axios.delete(`/api/users/${userID.value}`)
-            .then(() => {
-                users.value = users.value.filter(user => user.id !== userID.value)
-                $('#userDeleteFormModal').modal('hide');                
-                toastr.info('User deleted successfully!');
-            });
-    }
+    const userDeleted = (userId) => {
+        users.value = users.value.filter(user => user.id !== userId);
+    };
+    
 
     onMounted(() => {
         getUsers();
@@ -154,17 +144,13 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(user, index) in users" :key="user.id">
-                                <td>{{ index + 1 }}</td>
-                                <td>{{ user.name }}</td>
-                                <td>{{ user.email }}</td>
-                                <td>{{ formatDate(user.created_at) }}</td>
-                                <td>-</td>
-                                <td>
-                                    <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
-                                    <a href="#" @click.prevent="deleteUserForm(user)"><i class="fa fa-trash text-danger ml-2"></i></a>
-                                </td>
-                            </tr>
+                            <UserListItem v-for="(user, index) in users" 
+                                :key="user.id" 
+                                :user=user
+                                :index=index 
+                                @edit-user="editUser"
+                                @user-deleted="userDeleted"
+                            />
                         </tbody>
                     </table>
                 </div>
@@ -212,30 +198,6 @@
                         <button type="submit" class="btn btn-primary">Save</button>
                     </div>
                 </Form>                
-            </div>
-        </div>
-    </div>
-
-    <!-- Modal delete -->
-    <div class="modal fade" id="userDeleteFormModal" data-backdrop="static" tabindex="-1" role="dialog"
-        aria-labelledby="userFormModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="userFormModalLabel">
-                        <span>Delete user</span>
-                    </h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <h5>Are you sure you want to delete this user?</h5>
-                </div>  
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button @click.prevent="deleteUser" type="button" class="btn btn-primary">Delete user</button>
-                </div>            
             </div>
         </div>
     </div>
